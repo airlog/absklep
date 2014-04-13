503fc9e28852d6b389c1b28d609b076e7a6f23ea
Marta Mularczyk <marta.mularczyk7@gmail.com>
2014-04-13 22:42:11 +0200

    zmiana i usuwanie produktów

--- absklep/views.py
+++ absklep/views.py
@@ -506,7 +506,20 @@ def remove_product(pid):
     return redirect(url_for('modify_product'))
     
     
+@app.route('/panel/addparam/', methods=['GET', 'POST'])
+def addparam():
+    from flask import g, request
+    from .models import Property
+    from .util import read_form
     
+    if not g.current_user.is_authenticated() or g.current_user.__tablename__ != "Employees": return redirect(url_for('emplogin'))
+    
+    if request.method == 'POST':
+        app.db.session.add(Property(read_form('key'), read_form('val')))
+        app.db.session.commit()
+        flash('Dodano')
+    
+    return render_template('panel/addparam.html')
 
 
 @app.route('/panel/orders/')
